112ec6b3b1f1383e1c39a3d8a9f36dbfdf24d0dd
Frederox <69014593+FrederoxGit@users.noreply.github.com>
2022-07-09 16:56:48 +0100

    Remove merged files

--- BP/scripts/Main.ts
+++ BP/scripts/Main.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { WorldGenerator } from "./Generation/WorldGenerator";
 import { world } from "mojang-minecraft"
 
@@ -7,15 +6,4 @@ const worldGenerator = new WorldGenerator(16, 60, "seed");
 world.events.beforeChat.subscribe((beforeChat) => {
     const playerChunk = worldGenerator.LocationToChunkCoord(beforeChat.sender.location);
     worldGenerator.GeneratePlanet(beforeChat.sender, 3, playerChunk);
-=======
-import { WorldGenerator } from "./WorldGenerator";
-import { world } from "mojang-minecraft"
-import { ChunkCoord } from "./Chunk";
-
-const worldGenerator = new WorldGenerator(16, 60);
-
-world.events.beforeChat.subscribe((beforeChat) => {
-    const playerChunk = worldGenerator.LocationToChunkCoord(beforeChat.sender.location);
-    worldGenerator.GeneratePlanet(beforeChat.sender, 9, playerChunk);
->>>>>>> 0d6534ca49f2295589fc352fb69f8e08c0422053
 })
